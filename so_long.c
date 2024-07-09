5883b6782950e0864c76a1d4e8953a995b8a87db
Sanda Hajaina Ranaivoarisoa <hajainasanda@gmail.com>
2024-07-09 16:59:02 +0300

    add some modif

--- so_long.c
+++ so_long.c
@@ -12,6 +12,22 @@
 
 #include "so_long.h"
 
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
 int	main(int argc, char *argv[])
 {
 	char **map;
@@ -20,12 +36,30 @@ int	main(int argc, char *argv[])
 	{
 		map = NULL;
 		map = read_map(argv[1]);
-		int i = 0;
-		while (i < 6)
+
+/***********************************************************/
+		
+		void	*mlx;
+		void	*mlx_win;
+		t_data	img;
+
+		mlx = mlx_init();
+		mlx_win = mlx_new_window(mlx, 900, 600, "Hello world!");
+		img.img = mlx_new_image(mlx, 900, 600);
+		img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+									&img.endian);
+		int x = 5;
+		int y = 5;
+		while (x < 50)
 		{
-			printf("%s", map[i]);
-			i++;
+			my_mlx_pixel_put(&img, x++, y, 0x00FF0000);
+			my_mlx_pixel_put(&img, x, 50, 0x00FF0000);
 		}
+		mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+		mlx_loop(mlx);
+
+/**********************************************************/
+
 	}
 	return (0);
 }
