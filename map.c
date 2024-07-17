2c4790cf381af8bcbf24dedacd9cdd617a45e3c5
Sanda Hajaina Ranaivoarisoa <hajainasanda@gmail.com>
2024-07-17 14:59:40 +0300

    draw map

--- map.c
+++ map.c
@@ -12,51 +12,51 @@
 
 #include "so_long.h"
 
-int    count_map_line(char *map)
+int	count_map_line(char *map)
 {
-    int     line;
-    int     fd;
-    int     r;
-    char    *buf;
+	int		line;
+	int		fd;
+	int		r;
+	char	*buf;
 
-    fd = open(map, O_RDONLY);
-    if (fd < 0)
-        perror("error");
-    r = 1;
-    line = 1;
-    buf = malloc((BUFFER_SIZE + 1) * sizeof(char));
-    if (!buf)
-			return (free(buf), 0);
-    while (r != 0)
-    {
-        r = read(fd, buf, BUFFER_SIZE);
+	fd = open(map, O_RDONLY);
+	if (fd < 0)
+		perror("error");
+	r = 1;
+	line = 1;
+	buf = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buf)
+		return (free(buf), 0);
+	while (r != 0)
+	{
+		r = read(fd, buf, BUFFER_SIZE);
 		if (r < 0)
 			return (free(buf), 0);
-        buf[r] = '\0';
-        if (ft_strchr(buf, '\n'))
-            line++;
-    }
-    free(buf);
-    close(fd);
-    return (line);
+		buf[r] = '\0';
+		if (ft_strchr(buf, '\n'))
+			line++;
+	}
+	free(buf);
+	close(fd);
+	return (line);
 }
 
-char    **read_map(char *file)
+char	**read_map(char *file)
 {
-    int     fd;
-    int     line;
-    int     i;
-    char    **map;
+	int		fd;
+	int		line;
+	int		i;
+	char	**map;
 
-    line = count_map_line(file);
-    map = malloc(sizeof (char *) * line);
-    if (!map)
-        return (NULL);
-    fd = open(file, O_RDONLY);
-    if (fd < 0)
-        perror("error");
-    i = 0;
-    while (i < line)
-        map[i++] = ft_strjoin("", get_next_line(fd));
-    return (map);
+	line = count_map_line(file);
+	map = malloc(sizeof(char *) * line);
+	if (!map)
+		return (NULL);
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+		perror("error");
+	i = 0;
+	while (i < line)
+		map[i++] = ft_strjoin("", get_next_line(fd));
+	return (map);
 }
--- so_long.c
+++ so_long.c
@@ -12,56 +12,86 @@
 
 #include "so_long.h"
 
-typedef struct	s_xpm
+typedef struct s_xpm
 {
 	void	*bg;
 	void	*coin;
 	void	*door;
 	void	*player;
 	void	*wall;
-}				t_xpm;
+}			t_xpm;
 
-typedef struct	s_game
+typedef struct s_game
 {
 	void	*mlx;
 	void	*win;
 	char	**map;
+	int		width;
+	int		height;
 	t_xpm	xpm;
-}				t_game;
+}			t_game;
 
 void	load_xpm(void *mlx, t_xpm *xpm)
 {
-	int		width;
-	int		height;
+	int	width;
+	int	height;
 
 	xpm->bg = mlx_xpm_file_to_image(mlx, "assets/bg.xpm", &width, &height);
 	xpm->coin = mlx_xpm_file_to_image(mlx, "assets/coin.xpm", &width, &height);
 	xpm->door = mlx_xpm_file_to_image(mlx, "assets/door.xpm", &width, &height);
-	xpm->player = mlx_xpm_file_to_image(mlx, "assets/ninja.xpm", &width, &height);
+	xpm->player = mlx_xpm_file_to_image(mlx, "assets/ninja.xpm", &width,
+			&height);
 	xpm->wall = mlx_xpm_file_to_image(mlx, "assets/wall.xpm", &width, &height);
 }
 
-void	draw_map(char **map)
+t_game	init_game(char *file)
 {
-	void    *mlx;
-		void	*win;
-		t_xpm	xpm;
+	t_game	game;
+	int		x;
+	int		y;
 
-		mlx = mlx_init();
-		win = mlx_new_window(mlx, 900, 600, "so_long");
-		load_xpm(mlx, &xpm);
-	//printf ("%c\n", map[0][0]);
-	int x = 0;
-	int y = 0;
-	while (y < 6)
+	game.mlx = mlx_init();
+	game.map = read_map(file);
+	x = 0;
+	while (game.map[0][x] != '\n')
+		x++;
+	y = count_map_line(file);
+	printf("\n");
+	game.width = x;
+	game.height = y;
+	game.win = mlx_new_window(game.mlx, (game.width * 32), (game.height * 32),
+			"so_long");
+	load_xpm(game.mlx, &game.xpm);
+	return (game);
+}
+
+void	render_game(t_game game)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	y = 0;
+	while (y < game.height)
 	{
 		x = 0;
-		while (x < 34)
+		while (x < game.width)
 		{
-			if (map[x][y] == '1')
-			{
-				mlx_put_image_to_window(mlx, win, xpm.door, (x*32), (y*32));
-			}
+			if (game.map[y][x] == '0')
+				mlx_put_image_to_window(game.mlx, game.win, game.xpm.bg, x * 32,
+					y * 32);
+			if (game.map[y][x] == '1')
+				mlx_put_image_to_window(game.mlx, game.win, game.xpm.wall, x
+					* 32, y * 32);
+			if (game.map[y][x] == 'C')
+				mlx_put_image_to_window(game.mlx, game.win, game.xpm.coin, x
+					* 32, y * 32);
+			if (game.map[y][x] == 'E')
+				mlx_put_image_to_window(game.mlx, game.win, game.xpm.door, x
+					* 32, y * 32);
+			if (game.map[y][x] == 'P')
+				mlx_put_image_to_window(game.mlx, game.win, game.xpm.player, x
+					* 32, y * 32);
 			x++;
 		}
 		y++;
@@ -70,30 +100,15 @@ void	draw_map(char **map)
 
 int	main(int argc, char *argv[])
 {
-	char **map;
-
 	if (argc == 2)
 	{
-		map = NULL;
-		map = read_map(argv[1]);
+		/***********************************************************/
+		t_game game;
 
-		printf("%c\n", map[0][0]);
-
-/***********************************************************/
-		
-		void    *mlx;
-		void	*win;
-		t_xpm	xpm;
-
-		mlx = mlx_init();
-		win = mlx_new_window(mlx, 900, 600, "so_long");
-		load_xpm(mlx, &xpm);
-
-		draw_map(map);
-
-		mlx_loop(mlx);
-/**********************************************************/
-
+		game = init_game(argv[1]);
+		render_game(game);
+		mlx_loop(game.mlx);
+		/**********************************************************/
 	}
 	return (0);
 }
